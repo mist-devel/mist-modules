266d64cb6f077c8f49bdbcdb35d1ace7601bdbb0
Gyorgy Szombathelyi <gyurco@freemail.hu>
2020-12-20 16:23:34 +0100

    user_io: 64 bit status word support

--- user_io.v
+++ user_io.v
@@ -47,7 +47,7 @@ module user_io #(parameter STRLEN=0, parameter PS2DIV=100, parameter ROM_DIRECT_
 	output              scandoubler_disable,
 	output              ypbpr,
 	output              no_csync,
-	output reg   [31:0] status,
+	output reg   [63:0] status,
 	output reg    [6:0] core_mod, // core variant, sent before the config string is requested
 	// RTC data from IO controller
 	// sec, min, hour, date, month, year, day (BCD)
@@ -500,8 +500,8 @@ always @(posedge clk_sys) begin : cmd_block
 
 				8'h15: status <= spi_byte_in;
 
-				// status, 32bit version
-				8'h1e: if(abyte_cnt<5) status[(abyte_cnt-1)<<3 +:8] <= spi_byte_in;
+				// status, 64bit version
+				8'h1e: if(abyte_cnt<9) status[(abyte_cnt-1)<<3 +:8] <= spi_byte_in;
 
 				// core variant
 				8'h21: core_mod <= spi_byte_in[6:0];
